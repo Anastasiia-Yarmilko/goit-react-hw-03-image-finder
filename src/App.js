29df0ecf749dc2320d693a868b3529b4a80b619f
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-06-11 01:05:29 +0300

    Creates button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import Container from './components/Container';
+import Gallery from './components/Gallery/Gallery';
+import Button from './components/Button/Button';
+import Modal from './components/Modal/Modal';
+import Searchbar from './components/Searchbar/Searchbar';
+import Loader from './components/Loader/Loader';
 import pixabayApi from './services/pixabay.api';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
@@ -12,5 +19,82 @@ class App extends Component {
     error: null,
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.q !== this.state.q) {
+      this.fetchGallery();
+    }
+  }
+
+  onChangeQuery = query => {
+    this.setState({ q: query, page: 1, gallery: [], error: null });
+  };
+  fetchGallery = () => {
+    const { q, page } = this.state;
+    const options = { q, page };
+
+    this.setState({ isLoading: true });
+    pixabayApi
+      .fetchPixabayImgs(options)
+      .then(({ data }) => {
+        this.setState(prevState => ({
+          gallery: [...prevState.gallery, ...data.hits],
+          page: prevState.page + 1,
+        }));
+      })
+      .catch(error => this.setState({ error }))
+      .finally(() => {
+        this.setState({ isLoading: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
+  };
+
+  imgClick = largeImageURL => {
+    this.setState({
+      largeImage: largeImageURL,
+    });
+
+    this.toggleModal();
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    const { showModal, gallery, isLoading, error, largeImage } = this.state;
+    const shouldShowLoadMoreBtn = gallery.length > 0 && !isLoading;
+    return (
+      <Container>
+        {error && <h1>Try again!</h1>}
+        <Searchbar onSubmit={this.onChangeQuery} />
+        <Gallery showGallery={gallery} onImgClick={this.imgClick} />
+
+        {isLoading && <Loader />}
+        {shouldShowLoadMoreBtn && <Button onClick={this.fetchGallery} />}
+
+        {showModal && (
+          <Modal onClose={this.imgClick}>
+            <img src={largeImage} alt="" />
+          </Modal>
+        )}
+      </Container>
+    );
+  }
 }
+
+App.propTypes = {
+  gallery: PropTypes.array,
+  page: PropTypes.number,
+  q: PropTypes.string,
+  largeImage: PropTypes.string,
+  showModal: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
+};
+
 export default App;
